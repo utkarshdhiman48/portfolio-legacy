7a6d19fa114ef5a92da63b0d2d64f163cc37ad8e
utkarsh48 <utkarshdhiman48@gmail.com>
2020-07-04 21:54:23 +0530

    scrolling to right place

--- script/terminal.js
+++ script/terminal.js
@@ -12,33 +12,52 @@ let keywords = [
     new KeyWord("education", "Redirecting to his institutions...", "cyan", ""),
     new KeyWord("projects", "Redirecting to projects...", "cyan", ""),
     new KeyWord("connect", "You can connect here", "cyan", ""),
-    new KeyWord("about", "Here is something about him...", "cyan", "")
+    new KeyWord("about", "Here is something about him...", "cyan", ""),
+    new KeyWord("facebook", "Redirecting to facebook profile...", "cyan", ""),
+    new KeyWord("twitter", "Redirecting to twitter profile...", "cyan", ""),
+    new KeyWord("linkedin", "Redirecting to linkedin profile...", "cyan", ""),
+    new KeyWord("github", "Redirecting to github profile...", "cyan", ""),
+    new KeyWord("email", "Configuring pre email settings, Opening...", "cyan", ""),
 ];
+
+
+
+
+/////////////VARIABLES
+
 const utcolor="ut-color";
 const prefix = `<br><span class="terminal-preview-text"><span class="yellowgreen">mock@terminal</span><span class="sky-blue">~</span>$ `;
 const suffix = `</span>`;
 const terminalPreview = document.querySelector("div.terminal-preview");
 const terminalInput = document.querySelector("input.terminal-input");
 
+////////first line
 terminalPreview.innerHTML=`<span class="yellowgreen">mock@terminal</span><span class="sky-blue">~</span>$ Type 'ut help' to get a list of all commands${suffix}`;
 
-
-
+////////Enter key event listener
 terminalInput.addEventListener("keydown",(e)=>{
     let inputField = e.target;
     let prevCommands = terminalPreview.innerHTML;
-    if(e.key==="Enter"){
+    if(e.key==="Enter"){//////////////// find alternative for mobile
         let textChunks = inputField.value.split(" ");
         if((textChunks[0] ==="ut" || textChunks[0] === "UT") && textChunks.length === 2 ) {
             let [found] = keywords.filter((f)=>{
                 return f.command === textChunks[1];
             });
-            let output = `${prefix}<span class="${utcolor}">${textChunks[0]}</span> <span class="${found.clas}">${textChunks[1]}</span><br>${found.response}${suffix}`;
-            terminalPreview.innerHTML=`${prevCommands}${output}`;
+            if(!found) {
+                let output = `${prefix}"${inputField.value}" is not a valid command${suffix}`;
+                terminalPreview.innerHTML=`${prevCommands}${output}`;
+            }
+            else{
+                let output = `${prefix}<span class="${utcolor}">${textChunks[0]}</span> <span class="${found.clas}">${textChunks[1]}</span><br>${found.response}${suffix}`;
+                terminalPreview.innerHTML=`${prevCommands}${output}`;
+            }
+            terminalPreview.lastChild.scrollIntoView(false);
         }
         else{
             let output = `${prefix}"${inputField.value}" is not a valid command${suffix}`;
             terminalPreview.innerHTML=`${prevCommands}${output}`;
+            terminalPreview.lastChild.scrollIntoView(false);
         }
 
 
@@ -48,7 +67,7 @@ terminalInput.addEventListener("keydown",(e)=>{
 
 
 
-
+        
         inputField.value = "";
     }
 });
