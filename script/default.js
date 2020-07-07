d6da3ee872f30f571a535dae8c1699e3adad67b3
Utkarsh Dhiman <utkarshdiman48@gmail.com>
2020-07-07 22:17:41 +0530

    hide floating buttons

--- script/default.js
+++ script/default.js
@@ -1,10 +1,24 @@
+const scrollVisibleBtns = document.querySelector(".scroll-visible-btns");
+const main = document.querySelector("main");
+const rect = main.getBoundingClientRect();
 const superScroll =  document.querySelector(".super-scroll");
 
 window.addEventListener("scroll",()=>{
     if(window.scrollY>=260){
+        //show nav bar
         superScroll.style.display= "block";
+        //show buttons
+        
     }
     else{
+        //hide nav bar
         superScroll.style.display= "none";
+        //hide buttons
+    }
+    if(rect.bottom <= window.scrollY+window.innerHeight){
+        scrollVisibleBtns.style.flexDirection="row";
+    }
+    else{
+        scrollVisibleBtns.style.flexDirection="column";
     }
 })
