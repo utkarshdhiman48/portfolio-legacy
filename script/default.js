0821f234782a96b3143ea1283fd54704b6a1890b
Utkarsh Dhiman <utkarshdiman48@gmail.com>
2020-07-08 22:33:04 +0530

    make it new

--- script/default.js
+++ script/default.js
@@ -1,30 +1,11 @@
-const superScroll =  document.querySelector(".super-scroll");
-const hamburger = document.querySelector(".hamburger");
-const navRFc = document.querySelector(".nav-r-fc");
-
+const topFixed = document.querySelector(".top-fixed");
+const sticky = topFixed.offsetTop;
 
 window.addEventListener("scroll",()=>{
-    if(window.scrollY>=260){
-        //show nav bar
-        // superScroll.style.display= "block";
-        superScroll.style.top= 0;
-        //show buttons
-        
+    if (window.pageYOffset >= sticky) {
+        topFixed.classList.add("sticky")
     }
-    else{
-        //hide nav bar
-        // superScroll.style.display= "none";
-        superScroll.style.top= "-4rem";
-        //hide buttons
-    }
-});
-
-
-hamburger.addEventListener("click",()=>{
-    if(navRFc.style.display === "none" || navRFc.style.display === ""){
-        navRFc.style.display = "block";
-    }
-    else if(navRFc.style.display === "block"){
-        navRFc.style.display = "none";
+    else {
+        topFixed.classList.remove("sticky");
     }
 })
