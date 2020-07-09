dd95e9a3dff120b7f1e47593e699ae2e60fa3cd1
Utkarsh Dhiman <utkarshdiman48@gmail.com>
2020-07-09 13:20:16 +0530

    upto skills

--- script/default.js
+++ script/default.js
@@ -1,7 +1,13 @@
 const topFixed = document.querySelector(".top-fixed");
 const sticky = topFixed.offsetTop;
+const scrollProgressBar = document.querySelector("div.scroll-progress-bar");
 
 window.addEventListener("scroll",()=>{
+    const percent = window.scrollY/(document.body.scrollHeight - window.innerHeight)*100;
+    scrollProgressBar.style.width = `${percent}%`;
+
+
+
     if (window.pageYOffset >= sticky) {
         topFixed.classList.add("sticky")
     }
